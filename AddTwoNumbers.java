9e143fbe64d9af9da5027f2e964ad2141ec1406a
Abhijeet <abhijeets.94@gmail.com>
2021-02-22 23:02:25 +0530

    sum of n numbers

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 public class AddTwoNumbers {
 
     public static void main(String[] args){
-        
-        int num1, num2, sum;    
+
+        int n, sum = 0;
+        int num[] = new int[100];    
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter num1");
-        num1 = sc.nextInt();
-        System.out.println("Enter num2");
-        num2 = sc.nextInt();
-        sum = num1+num2;
+        System.out.println("Add number of terms to be added");
+        n = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            System.out.println("Enter number "+i);
+            num[i] = sc.nextInt();
+            sum = sum + num[i];
+        }
+        
         System.out.println("Result is "+sum);
         
     }
