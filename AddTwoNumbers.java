ccad659f798e787b97d3ab84393e226f36fd96ce
Abhijeet <abhijeets.94@gmail.com>
2021-02-24 02:53:18 +0530

    removed unwanted lines of code

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,31 +1,28 @@
 import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-public class AddTwoNumbers {
 
-    public static void main(String[] args){
- 
-        int result = 0 ;
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+        int result = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter numbers to be added seperated by ',' ");
         String num = sc.nextLine();
         String[] numArray = num.split(",");
 
-
-      try{
-              for (int i=0;i<numArray.length;i++) {
-                    result = result + Integer.parseInt(numArray[i]);
-                }
+        try {
+            for (int i = 0; i < numArray.length; i++) {
+                result = result + Integer.parseInt(numArray[i]);
             }
-           catch(Exception e) {
-               System.out.println("");
-           }
+        } catch (Exception e) {
+            System.out.println("");
+        }
 
-           if (result >=1000) {
-               result = result-1000;
+        if (result >= 1000) {
+            result = result - 1000;
 
-           }
-           
-            System.out.println("Result is "+result );  
+        }
+
+        System.out.println("Result is " + result);
         sc.close();
-    }}
+    }
+}
