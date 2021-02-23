a2e27ac364ed12364dc1309b9ba4aef9457aa05c
Abhijeet <abhijeets.94@gmail.com>
2021-02-24 01:52:50 +0530

    add unknown amount of number

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 public class AddTwoNumbers {
 
     public static void main(String[] args){
-
-        int n, sum = 0;
-        int num[] = new int[100];    
+ 
+        int result = 0 ;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Add number of terms to be added");
-        n = sc.nextInt();
-        for (int i=0; i<n; i++) {
-            System.out.println("Enter number "+i);
-            num[i] = sc.nextInt();
-            sum = sum + num[i];
+        System.out.println("Enter 2 numbers to be added");
+        String num = sc.nextLine();
+        String[] numArray = num.split(",");
+
+        for (int i=0;i<numArray.length;i++) {
+            result = result + Integer.parseInt(numArray[i]);
         }
-        
-        System.out.println("Result is "+sum);
-        
-    }
-}
+   
+        System.out.println("Result is "+result );   
+        sc.close();
+    }}
